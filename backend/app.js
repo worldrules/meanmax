bdb83f49eb1e298df9d5942a5c7f2b4fa5d9c99e
worldrulesdev <worldrulesdev@gmail.com>
2019-10-10 18:34:16 -0300

    oo

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res, next) => {
-  console.log('First Middleware');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req,res, next) => {
-  res.send('Hello from express !')
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully"
+   })
+})
+
+app.use('/api/posts', (req,res, next) => {
+  const posts = [
+    {
+      id: "1a3s2d13s2ad",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "1a3s2d13s2ad",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
+    }
+  ];
+  return res.status(200).json({
+    message: 'Posts fetched succefully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
