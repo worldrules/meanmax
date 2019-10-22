7e66ffb3802218e8a9f5ba800fd3d35acb79dfd2
worldrulesdev <worldrulesdev@gmail.com>
2019-10-22 15:32:31 -0300

    eoq

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,6 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
   post.save().then(createdPost => {
     res.status(201).json({
       message: "Post added successfully",
@@ -58,7 +57,7 @@ app.delete("/api/posts/:id", (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(result => {
     console.log(result);
     res.status(200).json({ message: "Post deleted!" });
-  }); 
+  });
 });
 
 module.exports = app;
